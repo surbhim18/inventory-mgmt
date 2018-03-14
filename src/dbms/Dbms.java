567cb347da71ae99ab019fca3ee2df6317087109
Surbhi Mittal <surbhim.mcs17.du@gmail.com>
2018-03-14 07:45:52 +0530

    Add files via upload

---
+++ src/dbms/Dbms.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms;
+
+import dbms.frames.MainFrame;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HP
+ */
+public class Dbms {    
+    
+    public static void main(String[] args) {
+        
+                    
+            MainFrame start = new MainFrame();
+            start.setVisible(true);
+        
+    }
+}
